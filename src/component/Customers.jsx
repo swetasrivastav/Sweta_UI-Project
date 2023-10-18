c02674116358545a53598a59f5230d3419b0aa2a
swetasrivastav <swetasri592@gmail.com>
2023-10-18 20:01:27 +0530

    chart

--- src/component/Customers.jsx
+++ src/component/Customers.jsx
@@ -21,7 +21,10 @@ const Customers = () => {
 <option >Yearly</option>
 </select>
 </div>
-<img src={chart} alt='' className='lg:w-[700px] lg:h-60 lg:ml-6 lg:p-1 p-3 lg:mt-4'/>
+<div>
+
+<img src={chart} alt='' className='lg:w-[700px] lg:h-60 lg:ml-6 p-1 w-[280px] ml-4  lg:mt-4'/>
+</div>
   </div>
 
     {/* customer card */}
